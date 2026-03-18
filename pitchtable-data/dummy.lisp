74ce1540f1db513064bbacb829051a74223b12a5
Johannes Keller <johannkell@gmail.com>
2026-03-18 23:35:38 +0100

    moving things around

--- pitchtable-data/dummy.lisp
+++
@@ -1,8 +0,0 @@
-#S(PITCH-TABLE
-   :RANK-LIST (#S(RANK
-                  :NAME-STRING "Salicional"
-                  :PIPE-LIST (#S(PIPE
-                                 :MIDI-PITCH 60
-                                 :PITCH-LIST (#S(PITCH
-                                                 :AFTERTOUCH 64
-                                                 :FREQUENCY NIL)))))))
